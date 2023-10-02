d72b34e20743eb84775f70c08015a341a56f6bca
Sothea <62893090+hassothea@users.noreply.github.com>
2023-10-02 22:03:37 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+DESCRIPTION = 'This is the python package for a kernel-based consensual aggregation by S. Has (2023)'
+with open('README.txt') as f:
+    LONG_DESCRIPTION = f.read()
+
+version = "0.0.1"
+setup(name='gradientcobra',
+      version=version,
+      description='Python implementation for Gradient COBRA: A kernel-based consensual aggregation for regression by S. Has (2023).',
+      author=['Sothea Has'],
+      author_email=['sothea.has@lpsm.paris'],
+      url='https://github.com/hassothea/gradientcobra',
+      packages=find_packages(),
+      install_requires=[
+          'numpy',
+          'pandas'
+          'scipy',
+          'scikit-learn',
+          'matplotlib',
+          'seaborn',
+          'pandas'
+
+      ],
+      test_suite='tests',
+      keywords=[
+          'Consensual aggregation',
+          'Kernel',
+          'Regression',
+          'Statistical Aggregation'
+      ],
+      long_description=LONG_DESCRIPTION)
