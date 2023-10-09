a1f03c6ea45421925d5effbac2468cc799487d44
Sothea <62893090+hassothea@users.noreply.github.com>
2023-10-09 14:14:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,41 +7,40 @@ from setuptools import setup, find_packages
 # Get the absolute path to the main directory (one level up)
 #main_dir = os.path.abspath(os.path.join(current_dir, ".."))
 
-DESCRIPTION = "This is the python package for a kernel-based consensual aggregation by S. Has (2023)"
-with open("README.rst") as f:
+DESCRIPTION = 'This is the python package for a kernel-based consensual aggregation by S. Has (2023)'
+with open('README.rst') as f:
     LONG_DESCRIPTION = f.read()
 
-version = "0.0.1"
-setup(name="gradientcobra",
+version = "1.0.0"
+setup(name='gradientcobra',
       version=version,
-      description="Python implementation for Gradient COBRA: A kernel-based consensual aggregation for regression by S. Has (2023).",
+      description='Python implementation for Gradient COBRA: A kernel-based consensual aggregation for regression by S. Has (2023).',
       author=["Sothea Has"],
-      author_email=["sothea.has@lpsm.paris"],
-      url="https://github.com/hassothea/gradientcobra/",
+      author_email="sothea.has@lpsm.paris",
+      url='https://github.com/hassothea/gradientcobra/',
       packages=["gradientcobra"],
       classifiers=[
-          "Development Status :: 4 - Beta",
-          "License :: OSI Approved :: MIT License",
-          "Programming Language :: Python",
-          "Operating System :: OS Independent :: Microsoft :: Windows",
-          "Intended Audience :: Science/Research",
-          "Topic :: Scientific/Engineering"
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Operating System :: Microsoft :: Windows',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering'
       ],
       install_requires=[
-          "numpy",
-          "pandas",
-          "scipy",
-          "scikit-learn",
-          "matplotlib",
-          "seaborn",
-          "pandas"
+          'numpy>=1.20.1',
+          'pandas>=2.1.0',
+          'scipy>=1.10.1',
+          'scikit-learn>=1.2',
+          'matplotlib',
+          'seaborn'
 
       ],
-      test_suite="tests",
+      test_suite='tests',
       keywords=[
-          "Consensual aggregation",
-          "Kernel",
-          "Regression",
-          "Statistical Aggregation"
+          'Consensual aggregation',
+          'Kernel',
+          'Regression',
+          'Statistical Aggregation'
       ],
       long_description=LONG_DESCRIPTION)
